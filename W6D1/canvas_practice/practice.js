20fd3c0b91aabfe9d66491f37a94a887fa31261d
jenkennedy <kennifer09@gmail.com>
2018-07-22 21:22:55 -0700

    Draw rectangle and circle using React

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  let canvas = document.getElementById('mycanvas');
+  canvas.width = 500;
+  canvas.height = 500;
+
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = "orange";
+  ctx.fillRect(0,0,500,700)
+
+  ctx.beginPath();
+  ctx.arc(200,200,100,0,2 * Math.PI);
+  ctx.strokeStyle ='white';
+  ctx.lineWidth = 5;
+  ctx.stroke();
 });
