7cf93977c2fcaeb2f1129d7ccc2599014696cab1
jenkennedy <kennifer09@gmail.com>
2018-07-29 20:12:00 -0700

    Create store for job listing app

---
+++ W7D1/JobListings/frontend/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  city: "Please Select",
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
